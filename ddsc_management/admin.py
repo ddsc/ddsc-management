aebb12682ddd9f9bac170d31991e126175b8755e
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-04-02 16:44:10 +0200

    Hide Piston from admin

--- ddsc_management/admin.py
+++ ddsc_management/admin.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from piston.models import Consumer, Nonce, Resource, Token
+
 from ddsc_management import models
+
+# Hide Piston:
+admin.site.unregister(Consumer)
+admin.site.unregister(Nonce)
+admin.site.unregister(Resource)
+admin.site.unregister(Token)
