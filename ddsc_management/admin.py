1e01698dd2f3d1558be500bb47577fad63eb93d5
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-04-09 14:21:29 +0200

    Fix typo

--- ddsc_management/admin.py
+++ ddsc_management/admin.py
@@ -94,7 +94,7 @@ class DdscUserAdmin(UserAdmin):
         local_user.email = sso_user.get('email')
         local_user.is_active = sso_user.get('is_active', False)
         local_user.is_superuser = sso_user.get('is_superuser', False)
-        local_user.is_superuser = sso_user.get('is_staff', False)
+        local_user.is_staff = sso_user.get('is_staff', False)
         local_user.date_joined = sso_user.get('created_at')
 
         # TODO: synchronize user profile as well?
